06e334f3397fb46f3fef79223977a52840037b35
Salome Toro <salome.torod@autonoma.edu.co>
2025-05-27 08:18:25 -0500

    Se creo el paquete views y la clase ManuPrincipal

--- src/autonoma/laberinto/views/MenuPrincipal.java
+++ src/autonoma/laberinto/views/MenuPrincipal.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package autonoma.Laberinto.models;
+package autonoma.Laberinto.views;
 
 /**
  *
  * @author Salo
  */
-public class Laberinto {
+public class MenuPrincipal {
     
 }
